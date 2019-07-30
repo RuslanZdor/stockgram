18b2ef8fae0acf8c7794a2d64647665ded714c34
RuslanZdor <ruslan_zdor@epam.com>
2019-07-29 21:15:54 -0500

    add missed class

---
+++ beans/src/main/java/com/stocker/yahoo/data/CompanyStats.java
@@ -0,0 +1,37 @@
+package com.stocker.yahoo.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+
+@Data
+@Log4j
+@NoArgsConstructor
+@Document
+public class CompanyStats {
+
+    private double marketCap;
+    private long sharesFloat;
+    private long sharesOutstanding;
+    private long sharesOwned;
+    private double eps;
+    private double pe;
+    private double peg;
+    private double epsEstimateCurrentYear;
+    private double epsEstimateNextQuarter;
+    private double epsEstimateNextYear;
+    private double priceBook;
+    private double priceSales;
+    private double bookValuePerShare;
+    private double revenue;
+    private double EBITDA;
+    private double oneYearTargetPrice;
+    private double shortRatio;
+    private LocalDate earningsAnnouncement;
+
+}
