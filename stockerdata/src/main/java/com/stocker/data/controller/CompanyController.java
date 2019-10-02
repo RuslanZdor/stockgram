9204aba751b0dc04e07c93b32a7cd126349d311a
rzdor <ruslan_zdor@epam.com>
2019-10-01 22:39:17 -0500

    add lgs

--- stockerdata/src/main/java/com/stocker/data/controller/CompanyController.java
+++ stockerdata/src/main/java/com/stocker/data/controller/CompanyController.java
@@ -4,6 +4,7 @@ import com.stocker.data.CompanyUtils;
 import com.stocker.data.spring.CompanyRepository;
 import com.stocker.data.spring.YahooDataClient;
 import com.stocker.yahoo.data.Company;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
 
+@Log4j2
 @RestController
 class CompanyController {
 
@@ -23,6 +25,7 @@ class CompanyController {
     @GetMapping("/company/{symbol}/")
     public Publisher<Company> findCompany(@PathVariable("symbol") String symbol) {
         Company company = repository.findFirstBySymbol(Mono.just(symbol.toUpperCase())).block();
+        log.info("need update " + CompanyUtils.needUpdate(company));
         if (CompanyUtils.needUpdate(company)) {
             yahooDataClient.updateCompany(symbol).block();
         }
