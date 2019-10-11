6188645dce629b81cd65517e0caccf122bbee29c
rzdor <ruslan_zdor@epam.com>
2019-10-10 21:12:44 -0500

    add strategy get controller

--- strategic-service/src/main/java/com/stocker/strategic/spring/StrategyResultRepository.java
+++ strategic-service/src/main/java/com/stocker/strategic/spring/StrategyResultRepository.java
@@ -3,7 +3,11 @@ package com.stocker.strategic.spring;
 import com.stocker.yahoo.data.StrategyResult;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface StrategyResultRepository extends ReactiveCrudRepository<StrategyResult, String> {
+
+    Flux<StrategyResult> findAllByStrategyName(Mono<String> strategyName);
 }
--- strategic-service/src/main/java/com/stocker/strategic/spring/controller/StrategyController.java
+++ strategic-service/src/main/java/com/stocker/strategic/spring/controller/StrategyController.java
@@ -1,10 +1,14 @@
 package com.stocker.strategic.spring.controller;
 
+import com.stocker.strategic.spring.StrategyResultRepository;
 import com.stocker.strategic.spring.calculation.DividendAristocratsCalculation;
+import com.stocker.yahoo.data.StrategyResult;
 import lombok.extern.log4j.Log4j2;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @Log4j2
 @RestController
@@ -13,8 +17,16 @@ class StrategyController {
     @Autowired
     private DividendAristocratsCalculation dividendAristocratsCalculation;
 
+    @Autowired
+    private StrategyResultRepository strategyResultRepository;
+
     @GetMapping("/strategy/dividendAristocrats")
-    public void dividendAristocrats() {
+    public Publisher<StrategyResult> dividendAristocrats() {
+        return strategyResultRepository.findAllByStrategyName(Mono.just("dividendAristocrats"));
+    }
+
+    @GetMapping("/strategy/dividendAristocrats/update")
+    public void updateDividendAristocrats() {
         dividendAristocratsCalculation.calculate();
     }
 
