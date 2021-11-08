f1b4a601e00ecd76120e2e4321bbcf094c7ae956
rzdor <ruslan_zdor@epam.com>
2021-11-08 12:52:21 -0600

    update only days that are wasn't finished yet

--- stockerdata/src/main/java/com/stocker/data/controller/market/UpdateMarketsHandler.java
+++ stockerdata/src/main/java/com/stocker/data/controller/market/UpdateMarketsHandler.java
@@ -3,9 +3,7 @@ package com.stocker.data.controller.market;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.inject.Injector;
-import com.stocker.data.dao.DayDAO;
 import com.stocker.data.dao.MarketDayDAO;
-import com.stocker.data.dao.StockDAO;
 import com.stocker.data.job.IMarketCalculateJob;
 import com.stocker.data.job.market.CalculateAllMarketFields;
 import com.stocker.data.module.DIFactory;
@@ -25,8 +23,6 @@ public class UpdateMarketsHandler implements RequestHandler<MarketUpdate, String
 
     private final IMarketCalculateJob calculateAllFields;
     private final MarketDayDAO marketDayDAO;
-    private final DayDAO dayDAO;
-    private final StockDAO stockDAO;
 
     public UpdateMarketsHandler() {
         this(DIFactory.init());
@@ -35,8 +31,6 @@ public class UpdateMarketsHandler implements RequestHandler<MarketUpdate, String
     public UpdateMarketsHandler(Injector injector) {
         calculateAllFields = injector.getInstance(CalculateAllMarketFields.class);
         marketDayDAO = injector.getInstance(MarketDayDAO.class);
-        dayDAO = injector.getInstance(DayDAO.class);
-        stockDAO = injector.getInstance(StockDAO.class);
     }
 
     public String handleRequest(MarketUpdate marketUpdate, Context context) {
@@ -46,8 +40,10 @@ public class UpdateMarketsHandler implements RequestHandler<MarketUpdate, String
         market.setDays(new ArrayList<>(marketDayDAO.findAllData(market.getSymbol())));
         calculateAllFields.calculate(market, lastDays);
         market.getDays()
+                .stream().filter(marketDay -> !marketDay.isFinished())
                 .forEach(day -> {
-                    day.setFinished(false);
+                    log.debug(String.format("Update %s market day %s", market.getSymbol(), day.getDateTimestamp()));
+                    day.setFinished(true);
                     marketDayDAO.save(day);
                 });
         return "SUCCESS";
